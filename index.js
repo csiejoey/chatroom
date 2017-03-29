f8ed5568d9ee33d9afe118b8a7925c6ee50462f4
csiejoey <asdfasdfasdf12tw@gmail.com>
2017-03-29 11:23:40 +0800

    3_28_chatwindow

--- index.js
+++ index.js
@@ -23,12 +23,21 @@ app.get('/friends', function(req, res) {
 	res.sendFile(__dirname + '/friends.html');
 });
 
+app.get('/history', function(req, res) {
+	res.sendFile(__dirname + '/history.html');
+});
+
 io.sockets.on('connection', function(socket) {
 	socket.on('addroom1user', function(username) {
 		socket.username = username;
 		socket.room = 'room1';
 		usernames[username] = username;
 		socket.join('room1');
+		// setting profile picture and account name
+		let yahPic = '<img id="opponentPic" src="lai.jpg">';
+		socket.emit('setpic', yahPic);
+		let yahAccount = '雅君 (cuteyah38)';
+		socket.emit('setaccount', yahAccount);
 	});
 
 	socket.on('addroom2user', function(username) {
@@ -36,6 +45,11 @@ io.sockets.on('connection', function(socket) {
 		socket.room = 'room2';
 		usernames[username] = username;
 		socket.join('room2');
+
+		let joeyPic = '<img id="opponentPic" src="joey.jpg">';
+		socket.emit('setpic', joeyPic);
+		let joeyAccount = '就已 (hjoey0607)';
+		socket.emit('setaccount', joeyAccount);
 	});
 
 	socket.on('sendchat', function(data){
@@ -53,7 +67,28 @@ io.sockets.on('connection', function(socket) {
 			data = '<a href="' + data + '">' + data + '</a>';
 		} else if (data === '叮咚，有人在家嗎～～～～') {
 			data = '<span style="color: red; font-weight: bold">叮咚，有人在家嗎～～～～</span>';
-		} // 字體顏色
+		} else if (data.indexOf('status') > -1) {
+			function splitString(stringToSplit, separator) {
+				arrayOfStrings = stringToSplit.split(separator);
+				let statusType = arrayOfStrings[1];
+				let opponentName = arrayOfStrings[2];
+				let statusWord = arrayOfStrings[3];
+				let blueOrNot = arrayOfStrings[4];
+				io.sockets.in(socket.room).emit('changestatus', statusType, opponentName, statusWord, blueOrNot);
+			}
+			splitString(data, ' ');
+			return;
+		} else if (data.indexOf('offlineTxt') > -1) {
+			function splitString(stringToSplit, separator) {
+				arrayOfStrings = stringToSplit.split(separator);
+				let offlineName = arrayOfStrings[1];
+				io.sockets.in(socket.room).emit('setofflinetxt', offlineName);
+			}
+			splitString(data, ' ');
+			return;
+		}
+
+		// 字體顏色
 		io.sockets.in(socket.room).emit('updatechat', socket.username, data);
 	});
 });
